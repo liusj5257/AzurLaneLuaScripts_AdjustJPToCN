9229bc71b13522c622c43526e85973aff0ab65ab
liu_shaojun <liu_shaojun@outlook.com>
2023-04-24 17:20:49 +0800

    gametip

---
+++ Adjust/gametip.cpp
@@ -0,0 +1,135 @@
+#include <unistd.h>
+
+#include <fstream>
+#include <iostream>
+#include <regex>
+#include <sstream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+#define attr "tip"
+string currentFileName = __FILE__;
+ofstream output_file;
+
+void readJP(vector<string> &allArray);
+void output(string id, char *attribute, string str);
+void writeCN(vector<string> &allArray, char *attribute);
+int main() {
+  int lastSlash = currentFileName.rfind('/');
+  if (lastSlash != string::npos) {
+    currentFileName = currentFileName.substr(
+        lastSlash + 1);  // get the filename without the path
+  }
+  int lastDot = currentFileName.rfind('.');
+  if (lastDot != string::npos) {
+    currentFileName =
+        currentFileName.substr(0, lastDot);  // remove the file extension
+  }
+  string OutputFileName = "..\\..\\Output\\" + currentFileName;
+  cout << OutputFileName << endl;
+  output_file.open(OutputFileName);
+
+  vector<string> allArray;
+
+  // readJP();
+  readJP(allArray);
+  writeCN(allArray, attr);
+  return 0;
+}
+
+void readJP(vector<string> &allArray) {
+  string filename = "..\\..\\JP\\sharecfgdata\\" + currentFileName + ".lua";
+
+  string line;
+  std::regex pattern("_G\\.pg\\.base\\.gametip\\.(\\w+)");
+
+  ifstream file(filename);
+  if (file.is_open()) {
+    while (getline(file, line)) {
+      std::smatch matches;
+      if (std::regex_search(line, matches, pattern)) {
+        for (size_t i = 1; i < matches.size(); i++) {
+          allArray.push_back(matches[i]);
+        }
+        // break;
+      }
+    }
+    file.close();
+  } else {
+    cout << "Unable to open file" << endl;
+  }
+
+  for (int i = 0; i < allArray.size(); i++) {
+    //      cout << allArray[i] << " ";
+  }
+  cout << "读取完毕" << endl;
+}
+
+void writeCN(vector<string> &allArray, char *attribute) {
+  string filename = "..\\..\\CN\\sharecfgdata\\" + currentFileName + ".lua";
+  string line;
+  ifstream file(filename);
+  if (file.is_open()) {
+    for (int i = 0; i < allArray.size(); i++) {
+      bool foundAttribute = false;
+      string searchString = allArray[i];
+      while (getline(file, line)) {
+        if (line.find(searchString) != string::npos) {
+          foundAttribute = true;
+          while (getline(file, line) && !line.empty()) {
+            string add = " = \"";
+            string attr_add = attribute + add;
+            if (line.find(attr_add) != std::string::npos) {
+              size_t start = line.find("\"") + 1;
+              size_t end = line.find("\"", start);
+              string name = line.substr(start, end - start);
+              // cout << name << endl;
+              output(allArray[i], attribute, name);
+              break;
+            } else if (line.find("[[") != std::string::npos) {
+              std::string name;
+              while (getline(file, line)) {
+                // bool flag = true;
+
+                if (line.find("]]") !=
+                    std::string::npos) {  // check if there is "]]" in the
+                                          // line
+                  // flag = false;
+                  break;
+                }
+                name += line + "\\n";
+              }
+              output(allArray[i], attribute, name);
+              break;
+            }
+            //info
+            break;
+          }
+          break;
+        }
+      }
+      if (!foundAttribute) {
+        file.clear();
+        file.seekg(0, ios::beg);
+      }
+    }
+    file.close();
+  } else {
+    cout << "Unable to open file" << endl;
+    // return 0;
+  }
+  //  return 1;
+}
+
+void output(string id, char *attribute, string str) {
+  static bool first = true;
+  if (first) {
+    first = false;
+    output_file << "#define gametip ";
+  }
+  string result = "replaceByAttribute(L,STR(\"" + id + "\"),STR(\"" +
+                  attribute + "\")" + ",STR(\"" + str + "\"));\\";
+  output_file << result << endl;
+}
--- JP/sharecfgdata/gametip.lua
+++ JP/sharecfgdata/gametip.lua
@@ -321,7 +321,7 @@ _G.pg.base.gametip.new_airi_error_code_200350 = {
 _G.pg.base.gametip.new_airi_error_code_200360 = {
 	tip = "決済システムへの接続に失敗しました"
 }
-
+newiri_error_code_300100
 _G.pg.base.gametip.new_airi_error_code_300100 = {
 	tip = "システムシェアに失敗しました"
 }

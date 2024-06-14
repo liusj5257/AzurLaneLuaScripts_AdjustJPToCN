70eed9031243ba9852e29c948dfecd27616693f5
liusj5257 <lius5257@gmail.com>
2024-06-14 10:27:24 +0800

    fix namecode

--- Adjust/namecode.py
+++ Adjust/namecode.py
@@ -1,15 +1,25 @@
 import os
 import re
+from lupa import LuaRuntime
+pg = {}
+lua = LuaRuntime(unpack_returned_tuples=True)
+lua.execute('''
+setmetatable(_G, {
+    __index = function(t, k)
+        rawset(t, k, {})
+        return rawget(t, k)
+    end
+})
+''')
 
 with open("../CN/sharecfg/name_code.lua", "r",encoding="utf-8") as file:
-    text = file.read()
-
-# 从文件中读取 namecode 数据
-namecodes = {}
-for match in re.finditer(r"id = (\d+),\s+name = \"(.*?)\",\s+code = \"(.*?)\"", text):
-    id, name, code = match.groups()
-    namecodes[int(id)] = name
-# 读取 test.txt 文件
+    lua_code = file.read()
+try:
+    lua.execute(lua_code)
+    print("Lua code executed successfully.")
+    name_code = lua.eval('_G.pg.name_code')
+except Exception as e:
+    print(f"An error occurred during Lua execution: {e}")
 
 
 folder_path = "../Output"  # 指定文件夹路径
@@ -25,7 +35,7 @@ for filename in files:
       text_to_replace = file.read()
 
   # 替换 {namecode:%d} 为对应的文本
-  text_to_replace = re.sub(r"{namecode:(\d+)}", lambda match: namecodes.get(int(match.group(1)), match.group(1)), text_to_replace)
+  text_to_replace = re.sub(r"{namecode:(\d+)(:[^}]*)?}", lambda match: name_code[int(match.group(1))]['name'] or 'error', text_to_replace)
 
   # 将替换后的文本保存到 ../Output/ship_skin_template.h 文件中
   with open(file_path, "w", encoding="utf-8") as file:
